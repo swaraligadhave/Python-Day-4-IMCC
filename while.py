07a4a01471c07d7a15b7ae6548cd846f715cecce
swaraligadhave <swaraligadhave07@gmail.com>
2026-09-14 10:01:18 +0530

    Day 4

--- while.py
+++ while.py
@@ -2,12 +2,18 @@
 #     string = input("Enter a string:")
 #     print(string)
 
-correct_pass = "some_pass"
-not_found = True
+# correct_pass = "some_pass"
+# not_found = True
 
-while not_found:
-    passw = input("Enter pass:")
-    if passw == correct_pass:
-        not_found = False
+# while not_found:
+#     passw = input("Enter pass:")
+#     if passw == correct_pass:
+#         not_found = False
 
-print("Password matched")        
+# print("Password matched")  
+
+i = 0
+while i < 10:
+    print(i)
+    i+=1
+    
