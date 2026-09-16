ba11082a59296b130713bf134ea8f199e9c8729e
swaraligadhave <swaraligadhave07@gmail.com>
2026-09-16 09:43:18 +0530

    Python basics

--- avg.py
+++ avg.py
@@ -1,2 +1,7 @@
 l = [1,2,3,4,5]
-print(sum(l)//len(l))
+avg=0
+sum=0
+for i in l:
+    sum+=i
+    avg=sum//len(l)
+print(avg)
--- sum.py
+++ sum.py
@@ -1,2 +1,5 @@
 l = [1,2,3,4,5]
-print(sum(l))
+sum=0
+for i in l:
+    sum+=i
+    print(sum)
